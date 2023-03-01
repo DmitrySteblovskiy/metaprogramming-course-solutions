eac551afea003ca06831155b02a40f45faa115f7
DmitrySteblovskiy <steblovskii.dk@phystech.edu>
2023-03-01 23:53:30 +0300

    fixing error again

--- task2/PolymorphicMapper.hpp
+++ task2/PolymorphicMapper.hpp
@@ -10,12 +10,10 @@ template <class From, auto target> struct Mapping {
   static constexpr auto Target{target};
 };
 
-template<class Base, class Target>
-struct PolymorphicMapper<Base, Target> {
-  static std::optional<Target> map(const Base &) {
-    return std::nullopt;
-  }
-};
+template<class Base, class Target, class... Mappings>
+struct PolymorphicMapper {};
+
+
 
 template <class Base, class Target, class... Mappings>
   requires(std::same_as<std::remove_cv_t<typename Mappings::Target>, Target> &&
@@ -34,5 +32,12 @@ struct PolymorphicMapper<Base, Target, Mapping<Caster, targ>, Mappings...> {
       return std::make_optional(targ);
     }
     return PolymorphicMapper<Base, Target, Mappings...>::map(object);
+  }
+};
+
+template<class Base, class Target>
+struct PolymorphicMapper<Base, Target> {
+  static std::optional<Target> map(const Base &) {
+    return std::nullopt;
   }
 };
