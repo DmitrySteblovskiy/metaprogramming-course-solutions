6b573de21dab8f56adceeffc1776a0c1eee57472
DmitrySteblovskiy <steblovskii.dk@phystech.edu>
2023-03-01 23:50:47 +0300

    trying to solve error(

--- task2/PolymorphicMapper.hpp
+++ task2/PolymorphicMapper.hpp
@@ -10,6 +10,13 @@ template <class From, auto target> struct Mapping {
   static constexpr auto Target{target};
 };
 
+template<class Base, class Target>
+struct PolymorphicMapper<Base, Target> {
+  static std::optional<Target> map(const Base &) {
+    return std::nullopt;
+  }
+};
+
 template <class Base, class Target, class... Mappings>
   requires(std::same_as<std::remove_cv_t<typename Mappings::Target>, Target> &&
            ...)
