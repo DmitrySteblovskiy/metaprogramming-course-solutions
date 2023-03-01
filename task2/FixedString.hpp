4616a3ece3c61f6d311e04ab6284a7973cf8aa8e
DmitrySteblovskiy <steblovskii.dk@phystech.edu>
2023-03-01 23:13:02 +0300

    another codestyle fix

--- task2/FixedString.hpp
+++ task2/FixedString.hpp
@@ -3,21 +3,20 @@
 #include <string>
 #include <string_view>
 
-template<size_t max_length>
-struct FixedString {
-  constexpr FixedString(const char* string, size_t length): len(length) {
-    for (size_t i = 0; i < len; ++ i)
-      impl[i] = string[i];
+template <size_t max_length> struct FixedString {
+  constexpr FixedString(const char *string, size_t length) : len(length) {
+    for (size_t i = 0; i < len; ++i)
+      res_str[i] = string[i];
   }
 
   constexpr operator std::string_view() const {
-    return std::string_view(impl, len);
+    return std::string_view(res_str, len);
   }
-  
+
   size_t len;
-  char impl[256];
+  char res_str[256];
 };
 
-constexpr FixedString<256> operator ""_cstr(const char* str, size_t len) {
+constexpr FixedString<256> operator""_cstr(const char *str, size_t len) {
   return FixedString<256>(str, len);
 }
