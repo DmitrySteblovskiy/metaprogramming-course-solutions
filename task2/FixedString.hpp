1e0077a7b134cb69d8d1b1cd3bfed0864603acc5
DmitrySteblovskiy <steblovskii.dk@phystech.edu>
2023-03-03 01:13:17 +0300

    fix codestyle

--- task2/FixedString.hpp
+++ task2/FixedString.hpp
@@ -4,6 +4,9 @@
 #include <string_view>
 
 template <size_t max_length> struct FixedString {
+  size_t len;
+  char res_str[256];
+
   constexpr FixedString(const char *string, size_t length) : len(length) {
     for (size_t i = 0; i < len; ++i)
       res_str[i] = string[i];
@@ -12,11 +15,8 @@ template <size_t max_length> struct FixedString {
   constexpr operator std::string_view() const {
     return std::string_view(res_str, len);
   }
-
-  size_t len;
-  char res_str[256];
 };
 
-constexpr FixedString<256> operator "" _cstr(const char *str, size_t len) {
+constexpr FixedString<256> operator"" _cstr(const char *str, size_t len) {
   return FixedString<256>(str, len);
 }
